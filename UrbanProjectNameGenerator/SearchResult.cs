3ccdaae6fde75c326a04713a5711f01eb2760153
Eric Irwin <=>
2014-11-20 09:13:55 -0700

    Implemented simple search client that parses Word, Definition and Example on UrbanDictionary Thesaurus

---
+++ UrbanProjectNameGenerator/SearchResult.cs
@@ -0,0 +1,11 @@
+﻿namespace UrbanProjectNameGenerator
+{
+    public class SearchResult
+    {
+        public string Word { get; set; }
+
+        public string Definition { get; set; }
+
+        public string Example { get; set; }
+    }
+}
